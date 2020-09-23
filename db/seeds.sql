0a388945f43df919e1fd54642b7c8335e88c1c64
kristiandh <kdwaynehill@gmail.com>
2020-09-23 08:16:07 -0400

    pushing completed files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Simple Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Morning Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Fantastic Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Island Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Supreme Burger', false);
